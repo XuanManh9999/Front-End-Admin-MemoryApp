3ac8dac7c57695060b1ba73fdd0150ca72147824
Xuan Manh 9999 <nguyenxuanmanh030929@gmail.com>
2025-10-14 08:58:20 +0700

    Update

--- src/configs/ConfigAxios.tsx
+++ src/configs/ConfigAxios.tsx
@@ -31,11 +31,7 @@ configApi.interceptors.response.use(
 
       try {
         const res = await axios.post(
-<<<<<<< HEAD
           "https://codezen.io.vn/admin/auth/refresh-token",
-=======
-          "https://backend-menory-app.vercel.app/admin/auth/refresh-token",
->>>>>>> 7f54a138569209256c6e76f5425dbbd27bae5a1d
           {},
           {
             headers: {
